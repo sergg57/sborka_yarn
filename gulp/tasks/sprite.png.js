1cfe8f7e33a5f4d51251cfe090ef103e8bd4b4bd
Sergey Grishin <sergg57@list.ru>
2017-04-16 18:52:01 +0300

    third commit

--- gulp/tasks/sprite.png.js
+++ gulp/tasks/sprite.png.js
@@ -4,7 +4,7 @@ module.exports = function () {
     $.gulp.task('sprite:png', function () {
       return $.gulp.src('./source/sprite/*.png')// папка откуда забираем
       .pipe($.gp.spritesmith({
-        imgName: '/sprite.png',
+        imgName: 'sprite.png',
         cssName: '../css/sprite.css'
       }))
     //.pipe($.gulp.dest('./build/assets/img'));
